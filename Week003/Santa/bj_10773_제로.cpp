923f02b89af773eb4647f76907bb39733fcc7ea7
ChrisP-00 <jspark4151@gmail.com>
2024-01-29 23:10:36 +0900

    update: bj_10773_제로.cpp

---
+++ Week003/Santa/bj_10773_제로.cpp
@@ -0,0 +1,34 @@
+#include <iostream> 
+#include <stack>
+#define fastTime ios::sync_with_stdio(false), cin.tie(0);
+
+using namespace std; 
+
+int main()
+{
+    fastTime;
+
+    stack<int> myStack;
+    int k, n, result = 0;
+
+    cin >> k;
+
+    while(k--)
+    {
+        cin >> n;
+
+        n == 0 ? myStack.pop() : myStack.push(n);
+    }
+
+    k = myStack.size();
+
+    while(k--)
+    {
+        result += myStack.top();
+        myStack.pop();
+    }
+
+    cout << result;
+
+    return 0;
+}
